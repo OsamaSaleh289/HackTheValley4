54be3e2dca64b29eb1ef8a4e2ed494d4895f1d6b
Asjad <asjad_2@hotmail.com>
2020-02-15 16:24:50 -0500

    checking

---
+++ frontend/app/src/main/java/com/example/bubbler/BubblePage.java
@@ -0,0 +1,14 @@
+package com.example.bubbler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BubblePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bubble_page);
+    }
+}
--- frontend/app/src/main/java/com/example/bubbler/BubbleView.java
+++ frontend/app/src/main/java/com/example/bubbler/BubbleView.java
@@ -1,14 +1,39 @@
 package com.example.bubbler;
 
+import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.view.animation.LinearInterpolator;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BubbleView extends AppCompatActivity{
+
+  private BubbleModel model;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.bubble);
+
+      final TextView first = (TextView) findViewById(R.id.first);
+      final TextView second = (TextView) findViewById(R.id.second);
+
+      final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
+      animator.setRepeatCount(ValueAnimator.INFINITE);
+      animator.setInterpolator(new LinearInterpolator());
+      animator.setDuration(9000L);
+      animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+          @Override
+          public void onAnimationUpdate(ValueAnimator animation) {
+              final float progress = (float) animation.getAnimatedValue();
+              final float width = first.getWidth();
+              final float translationX = width * progress;
+              first.setTranslationX(translationX);
+              second.setTranslationX(translationX - width);
+          }
+      });
+      animator.start();
   }
 
 }
