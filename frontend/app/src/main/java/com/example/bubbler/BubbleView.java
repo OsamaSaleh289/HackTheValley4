58b0fdabc168c4421f5d5996ed7c1c4d2e5c15cc
Asjad <asjad_2@hotmail.com>
2020-02-15 17:28:29 -0500

    xml

--- frontend/app/src/main/java/com/example/bubbler/BubbleView.java
+++ frontend/app/src/main/java/com/example/bubbler/BubbleView.java
@@ -14,21 +14,20 @@ public class BubbleView extends AppCompatActivity{
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.bubble);
-
+    setContentView(R.layout.activity_bubble_page);
       final TextView first = (TextView) findViewById(R.id.first);
       final TextView second = (TextView) findViewById(R.id.second);
 
-      final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
+      ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
       animator.setRepeatCount(ValueAnimator.INFINITE);
       animator.setInterpolator(new LinearInterpolator());
       animator.setDuration(9000L);
       animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
           @Override
           public void onAnimationUpdate(ValueAnimator animation) {
-              final float progress = (float) animation.getAnimatedValue();
-              final float width = first.getWidth();
-              final float translationX = width * progress;
+              float progress = (float) animation.getAnimatedValue();
+              float width = first.getWidth();
+              float translationX = width * progress;
               first.setTranslationX(translationX);
               second.setTranslationX(translationX - width);
           }
--- frontend/app/src/main/java/com/example/bubbler/MainActivity.java
+++ frontend/app/src/main/java/com/example/bubbler/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.bubbler;
 
+import android.content.Intent;
+import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +14,12 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    Date currentTime = Calendar.getInstance().getTime();
+    System.out.println(currentTime);
+  }
 
-    Button enter = findViewById(R.id.enter);
-    enter.setOnClickListener(new MenuController(this));
+  public void bubblePage(View view){
+    Intent intent = new Intent(this, BubbleView.class);
+    startActivity(intent);
   }
 }
