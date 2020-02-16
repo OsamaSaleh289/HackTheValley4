e21243209dddb2d9153245b326b5ce552dd6d4eb
Phillip Pham <phoneqw@gmail.com>
2020-02-15 20:17:53 -0500

    worked on location

--- frontend/app/src/main/java/com/example/bubbler/BubbleView.java
+++ frontend/app/src/main/java/com/example/bubbler/BubbleView.java
@@ -1,13 +1,22 @@
 package com.example.bubbler;
 
 import android.animation.ValueAnimator;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.animation.LinearInterpolator;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class BubbleView extends AppCompatActivity{
+
+  private FusedLocationProviderClient fusedLocationClient;
+  private Location location;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +44,20 @@ public class BubbleView extends AppCompatActivity{
       });
       animator.start();
 
-      first.setText(model.getLocationStr());
-  }
+    fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+    fusedLocationClient.getLastLocation()
+        .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+          @Override
+          public void onSuccess(Location location) {
+            // Got last known location. In some rare situations this can be null.
+            if (location != null) {
+              // Logic to handle location object
+              first.setText(
+                  "Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+            }
+          }
+        });
 
+    fusedLocationClient.getLastLocation();
+  }
 }
